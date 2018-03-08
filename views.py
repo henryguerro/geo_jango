e2d4de1a89286b2bd7fec99080c48c79380351b7
Henry Guerro <henryguerro@gmail.com>
2018-03-08 00:15:21 -0300

    Template Tags/filter

--- views.py
+++ views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import View
+from django.views.generic import TemplateView
 
 
-class HomeView(View):
+class HomeView(TemplateView):
+    template_name = 'countries/home.html'
 
-    def get(self, request, *args, **kwargs):
-        return render(request, 'countries/home.html')
+    def get_context_data(self, **kwargs):
+        ar = {'name': 'Argentina', 'code': 'AR'}
+        co = {'name': 'Colombia', 'code': 'CO'}
+        ve = {'name': 'Venezuela', 'code': 'VE'}
+
+        return {'countries': [ar, co, ve]}
 
 
+class TagsView(TemplateView):
+    template_name = 'countries/tags.html'
+
+    def get_context_data(self, **kwargs):
+        return {'population': '50'}
